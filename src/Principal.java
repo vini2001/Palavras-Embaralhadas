e0acede35d663101f56ee302abb988af51cb0442
Otavio de Meira Lima <otaviomeira2008@hotmail.com>
2022-11-06 15:39:32 -0300

    Principal#iniciar refactor

--- src/Principal.java
+++ src/Principal.java
@@ -6,30 +6,19 @@ public class Principal {
 
 	boolean finish = false;
 
+	static Scanner tec = new Scanner(System.in);
+
 	public void iniciar()  throws IOException {
 		
 		ArmazenarPalavras.armazenar();
-		Scanner tec = new Scanner(System.in);
 
-		System.out.println("Bem vindo ao jogo das Palavras embaralhadas!\n");
-		int op = 0;
-
-		do {
-			opcoes();
-			try {
-				op = tec.nextInt();
-			} catch (InputMismatchException i) {
-				op = 0;
-				tec.nextLine();
-			}
-			if (op < 1 || op > 3) {
-				System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nOpção inválida.\n");
-			}
-		} while (op < 1 || op > 3);
+		int op = selecionarModoJogo();
 
 		String modo = FabricaMecanicaDoJogo.getNomeMec(op);
 		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nVocê escolheu o modo " + modo + ".");
+
 		MecanicaDoJogo mec = FabricaMecanicaDoJogo.getMecanica(op);
+
 		mec.inicializar(new Callback() {
 			@Override
 			public void call() {
@@ -50,23 +39,53 @@ public class Principal {
 			System.out.println("Desembaralhe a palavra: " + wordEmb);
 			System.out.print("Resposta: ");
 			String resp = tec.next();
-			if (mec.Adivinhou(resp)) {
-				System.out.print("\nAcertou!");
-				System.out.print(" Digite qualquer coisa para continuar: ");
-				tec.next();
-			} else {
-				System.out.print("\nErrou!");
-				System.out.print(" Digite qualquer coisa para continuar: ");
-				tec.next();
-			}
+			calcularResposta(mec, resp);
 
 		}
+
+		encerrarJogo(mec);
+	}
+
+
+
+	private int selecionarModoJogo(){
+
+		System.out.println("Bem vindo ao jogo das Palavras embaralhadas!\n");
+		mostrarOpcoes();
+
+		int op = 0;
+		String opString;
+
+		while (op < 1 || op > 3){
+			opString = tec.next();
+			op = Integer.parseInt(opString);
+		}
+
+		return op;
+	}
+
+
+	private static void calcularResposta(MecanicaDoJogo mec, String resp) {
+
+		if (mec.Adivinhou(resp)) {
+			System.out.print("\nAcertou!");
+			System.out.print(" Digite qualquer coisa para continuar: ");
+			tec.next();
+		} else {
+			System.out.print("\nErrou!");
+			System.out.print(" Digite qualquer coisa para continuar: ");
+			tec.next();
+		}
+	}
+	
+	
+	private static void encerrarJogo(MecanicaDoJogo mec) {
 
 		System.out.println(mec.msgFim());
 		System.out.println("Jogo encerrado!    Acertou: " + mec.getCorretas() + "   Errou: " + mec.getErradas());
 		tec.close();
 	}
-
+	
 	public static void main(String[] args)  {
 		Principal p = new Principal();
 		try {
@@ -76,11 +95,11 @@ public class Principal {
 		}
 	}
 
-	public static void opcoes() {
+	public static void mostrarOpcoes() {
 		System.out.println("Escolha seu modo de jogo: ");
 		System.out.println("Modo fácil: Digite 1 para jogar com 2 vidas até zerar o jogo ou perder.");
 		System.out.println("Modo difícil: Digite 2 para jogar sem vidas até zerar o jogo ou perder. ");
 		System.out.println("Modo treino: Digite 3 para jogar com vidas infinitas, treinando.");
-		System.out.print("Opção: ");
+		System.out.print("Opção: ");
 	}
 }
