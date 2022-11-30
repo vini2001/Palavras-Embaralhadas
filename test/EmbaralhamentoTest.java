969534b9d122db306673ee82bf7df0441e85160b
Vinícius Assis Neves <viniciusassisneves@yahoo.com.br>
2022-11-29 23:58:51 -0300

    some unit tests

--- test/EmbaralhamentoTest.java
+++ test/EmbaralhamentoTest.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.assertNotEquals;
+
+import java.util.Arrays;
+
 import org.junit.*;
 import junit.framework.TestCase;
 
@@ -9,7 +12,26 @@ public class EmbaralhamentoTest extends TestCase {
 											// testar é se retorna uma palvra diferente da original, ou seja,
 											// embaralhada
 		EmbaralhadorAleatorio emb = new EmbaralhadorAleatorio();
-		assertNotEquals(emb.embaralhar("Papai"), "Papai");
+
+		for(int i = 0; i < 10; i++) {
+			String randomString = "";
+			for(int j = 0; j < 10; j++) {
+				randomString += (char) (Math.random() * 26 + 'a');
+			}
+			String embStr = emb.embaralhar(randomString);
+			assertNotEquals(embStr, randomString);
+
+			// sort randomString chars
+			char[] chars = randomString.toCharArray();
+			Arrays.sort(chars);
+			String sortedString = new String(chars);
+
+			chars = embStr.toCharArray();
+			Arrays.sort(chars);
+			String sortedEmbString = new String(chars);
+
+			assertEquals(sortedEmbString, sortedString);
+		}
 	}
 
 	@Test
@@ -17,7 +39,14 @@ public class EmbaralhamentoTest extends TestCase {
 											// testar é se retorna uma palvra diferente da original, ou seja,
 											// embaralhada
 		EmbaralhadorDuasPartes emb = new EmbaralhadorDuasPartes();
-		assertNotEquals(emb.embaralhar("Mamãe"), "Mamãe");
+		for(int i = 0; i < 10; i++) {
+			String randomString = "";
+			for(int j = 0; j < 10; j++) {
+				randomString += (char) (Math.random() * 26 + 'a');
+			}
+			String embStr = emb.embaralhar(randomString);
+			assertNotEquals(embStr, randomString);
+		}
 	}
 
 	@Test
@@ -25,6 +54,55 @@ public class EmbaralhamentoTest extends TestCase {
 		EmbaralhadorContrario emb = new EmbaralhadorContrario();
 		assertEquals(emb.embaralhar("MousePad"), "dapesuom");
 		assertEquals(emb.embaralhar("Vinícius"), "suicíniv");
+		assertEquals(emb.embaralhar("Casa"), "asac");
+		assertEquals(emb.embaralhar("Tapete"), "etepat");
+		assertEquals(emb.embaralhar("Cadeira"), "ariedac");
+		assertEquals(emb.embaralhar("Luminária"), "airánimul");
+		assertEquals(emb.embaralhar("Computador"), "rodatupmoc");
+		assertEquals(emb.embaralhar("Teclado"), "odalcet");
+		assertEquals(emb.embaralhar("Mouse"), "esuom");
+		assertEquals(emb.embaralhar("Monitor"), "rotinom");
+		assertEquals(emb.embaralhar("Notebook"), "koobeton");
+		assertEquals(emb.embaralhar("Impressora"), "arosserpmi");
+		assertEquals(emb.embaralhar("Cadeado"), "odaedac");
+	}
+
+	@Test
+	public void testGetEmbaralhadorReturnsValid() {
+		for(int i = 0; i < 10; i++) {
+			Embaralhador emb = FabricaEmbaralhadores.getEmbaralhador();
+			assertTrue(emb instanceof Embaralhador);
+		}
+	}
+
+	@Test
+	public void testFabricaMecanica() {
+		MecanicaDoJogo facil = FabricaMecanicaDoJogo.getMecanica(1);
+		assertEquals(FabricaMecanicaDoJogo.getNomeMec(1), "fácil");
+		assertTrue(facil instanceof MecanicaDoJogo);
+
+		MecanicaDoJogo medio = FabricaMecanicaDoJogo.getMecanica(2);
+		assertEquals(FabricaMecanicaDoJogo.getNomeMec(2), "difícil");
+		assertTrue(medio instanceof MecanicaDoJogo);
+
+		MecanicaDoJogo dificil = FabricaMecanicaDoJogo.getMecanica(3);
+		assertEquals(FabricaMecanicaDoJogo.getNomeMec(3), "treino");
+		assertTrue(dificil instanceof MecanicaDoJogo);
+	}
+
+	@Test 
+	public void testMecanicaVidas() {
+		MecanicaDoJogo facil = FabricaMecanicaDoJogo.getMecanica(1);
+		facil.inicializar(null);
+		assertEquals(facil.getVidas(), 2);
+
+		MecanicaDoJogo dificil = FabricaMecanicaDoJogo.getMecanica(2);
+		dificil.inicializar(null);
+		assertEquals(dificil.getVidas(), 0);
+
+		MecanicaDoJogo treino = FabricaMecanicaDoJogo.getMecanica(3);
+		treino.inicializar(null);
+		assertTrue(treino.getVidas() > 1000);
 	}
 
 }
