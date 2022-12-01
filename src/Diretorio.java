2ae19cfb7148bfee65d5d0e24e2d24ca23ee066c
Vinícius Assis Neves <viniciusassisneves@yahoo.com.br>
2022-11-30 21:44:07 -0300

    change directory to work with github actions

--- src/Diretorio.java
+++ src/Diretorio.java
@@ -2,8 +2,9 @@ import java.io.File;
 
 public class Diretorio {
     public static String dir() {
-        String userHome = System.getProperty("user.home");
-        String caminho = String.format("%s/Desktop/Palavras", userHome);
+        // String userHome = System.getProperty("user.home");
+        // String caminho = String.format("%s/Desktop/Palavras", userHome);
+        String caminho = ".Palavras";
         File file = new File(caminho);
         if (!file.exists())
             file.mkdir();
